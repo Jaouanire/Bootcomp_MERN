0591cec9db5c419df79dade34fabf810d7f4169b
Jaouanire <Jaouani111@gmail.com>
2024-03-25 00:25:35 +0000

    email challenge react

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Email from "./Email";
+
+function numberOfHours(number) {
+  return number;
+}
+
+function App() {
+  return <Email name='Jaouani Reda' numberOfHours={numberOfHours(10)} />
+}
+
+export default App
