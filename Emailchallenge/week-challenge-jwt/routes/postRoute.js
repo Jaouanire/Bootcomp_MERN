c11a4e7dd927b92dcb1647842908d9fd12dbdeb7
Jaouanire <Jaouani111@gmail.com>
2024-03-25 01:00:37 +0000

    Updated modifications

--- Emailchallenge/week-challenge-jwt/routes/postRoute.js
+++
@@ -1,7 +0,0 @@
-const { getPosts , addPost , updatePost , deletePost} = require('../controllers/postController')
-const postRouter = require('express').Router()
-const verifyToken = require('../middlewares/authenticate')
-postRouter.use(verifyToken)
-postRouter.get('/',getPosts).post('/', addPost).put('/:title', updatePost).delete('/', deletePost)
-
-module.exports = postRouter
